2f1e960d035a616ed7763d9e6c13e654f698aae5
CM <corentin.mailler19@gmail.com>
2022-11-03 18:19:48 +0100

    ajout du switchThemeBtn + ajout du type IMusicProps + modif du MusicItem

--- src/components/CustomNavbar.tsx
+++ src/components/CustomNavbar.tsx
@@ -1,11 +1,19 @@
+import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { ThemeContext } from '../context/themeContext/themeContext';
+import SwitchThemeBtn from './SwitchThemeBtn';
 
-const CustomNavbar = () => {
+const CustomNavbar = () => { 
+
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
+
     return ( 
-        <Navbar expand="lg" variant="dark" bg="dark">
+        <Navbar expand="lg" variant={`${darkMode ? "dark" : "light"}`} bg={`${darkMode ? "dark" : "light"}`}>
             <Container>
                 <Navbar.Brand href="/">YCP</Navbar.Brand>
+                <SwitchThemeBtn />
             </Container>
         </Navbar>
      );
